0c1e1a9fce86e30c6cf851f331e45f530fc3fa9e
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-05-28 14:04:10 -0600

    feat: initial project upload
    - add basics types

---
+++ src/python-annotations/basic_types.py
@@ -0,0 +1,12 @@
+username: str = 'Cris'
+altura: float = 1.6
+edad: int = 10
+active: bool = True
+
+username1: str
+altura1: float
+
+username1= 'Cris 1'
+altura= 1.61
+
+print(username, altura, edad, active)
