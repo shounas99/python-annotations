e260c8676e3a16a50e81c6de5ee37112cffb44ef
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-05-29 10:50:14 -0600

    feat: add literals
    - add literal types to indicate that a variable has a specific and concrete value

---
+++ src/python_annotations/literals.py
@@ -0,0 +1,11 @@
+from typing import Literal
+
+# Enum
+def set_background_color(color: Literal['red', 'green', 'yellow']):
+    pass
+
+def make_user(username: str, email: str, gender: Literal['F', 'M'])
+    pass
+    
+def opend_handler(file: str, mode: Literal['r', 'rb', 'w'])
+    pass
